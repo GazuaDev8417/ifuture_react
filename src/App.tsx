278eac34b24041dc49bb4860b6a8861ccb60d603
GazuaDev8417 <francadasilvaflamarion@gmail.com>
2024-10-06 19:56:23 -0300

    finalizando atualizações para deploy

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { createGlobalStyle } from "styled-components"
 
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+  
   *{
     margin: 0;
     padding: 0;
@@ -22,6 +24,10 @@ const GlobalStyle = createGlobalStyle`
   html::-webkit-scrollbar-thumb{
     background: #b11717;
     border-radius: 5rem;
+  }
+
+  body{
+    font-family: "Roboto", sans-serif;
   }
 
   input{
