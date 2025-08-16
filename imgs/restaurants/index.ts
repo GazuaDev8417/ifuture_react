5a27ac9daa116b13ce91b6b0c203b1ddd737a0b5
GazuaDev8417 <francadasilvaflamarion@gmail.com>
2025-08-16 12:03:13 -0300

    Updates

---
+++ imgs/restaurants/index.ts
@@ -0,0 +1,14 @@
+import suapizzaria from './pizzaria.png'
+import suadelicatessen from './delicatessen.png'
+import suachocolateria from './chocolate.png'
+import sualanchonete from './fastfood.png'
+
+
+
+export const restaurantImages:Record<string, string> = {
+    suapizzaria,
+    suadelicatessen,
+    suachocolateria,
+    sualanchonete
+}
+
